1267017b00f52c303de6ddca20bb8ceddf3d55a1
Maria Stepanova <116209851+MNJMARIA@users.noreply.github.com>
2023-05-26 22:19:36 +0300

    Add files

---
+++ init_db.sql
@@ -0,0 +1,33 @@
+CREATE TABLE worker (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(1000) NOT NULL,
+	birthday DATE CHECK(YEAR(birthday)>1900),
+	level ENUM('Trainee', 'Junior', 'Middle', 'Senior') NOT NULL,
+	salary INT CHECK(salary>=100 AND salary<=100000)
+);
+
+CREATE TABLE client(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(1000) NOT NULL CHECK(LENGTH(name)>=2 AND LENGTH(name)<=1000)
+);
+
+CREATE TABLE project(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	start_date DATE NOT NULL,
+	finish_date DATE NOT NULL,
+        client_id BIGINT NOT NULL
+);
+
+ALTER TABLE project 
+ADD CONSTRAINT client_id_fk 
+FOREIGN KEY(client_id) 
+REFERENCES client(id);
+
+CREATE TABLE project_worker (
+	project_id BIGINT NOT NULL,
+	worker_id BIGINT NOT NULL,
+	PRIMARY KEY (worker_id, project_id),
+	FOREIGN KEY (worker_id) REFERENCES worker (id),
+	FOREIGN KEY (project_id) REFERENCES project (id)
+);
+
